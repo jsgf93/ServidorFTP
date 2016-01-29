b3db839dc31b641892c4955c3cbd0f7c3fa00a49
jsgf93 <santiagogf11@gmail.com>
2016-01-29 15:28:16 -0500

    Create FTPDownloadFIle.java

---
+++ FTPDownloadFIle.java
@@ -0,0 +1,51 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class FTPDownloadFIle {
+
+	public static void main(String[] args) {
+		String server = "192.168.184.136";
+		int port = 21;
+		String user = "user";
+		String pass = "administrador";
+
+		FTPClient ftpClient = new FTPClient();
+		try {
+
+			ftpClient.connect(server, port);
+			ftpClient.login(user, pass);
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+			ftpClient.setRemoteVerificationEnabled(false);
+			String remoteFile1 = "/home/user/Desktop/harmonic.c";
+			File downloadFile1 = new File("/home/santiago/Desktop/harmonic1.c");
+			OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+			boolean success = ftpClient.retrieveFile(remoteFile1, outputStream1);
+			outputStream1.close();
+
+			if (success) {
+				System.out.println("File #1 has been downloaded successfully.");
+			}
+		} catch (IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
